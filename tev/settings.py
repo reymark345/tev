4c4f174cfeb72eea2152700cb4330624467e4af4
Reymark <reymarkvaldehueza@gmail.com>
2023-09-12 09:50:33 +0800

    feature/8: test

--- tev/settings.py
+++ tev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e&691z)oh*#_&yv(h^3sn^*=gxs09(%(5395)=xe+_im_bk^l&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
