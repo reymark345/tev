b8bdb9ed9db85b1a722e05b871ad62d385b2d579
Reymark N. Valdehueza <rnvaldehueza@ENTDSWD.LOCAL>
2024-10-23 16:43:19 +0800

    feature/56:

--- tev/asgi.py
+++ tev/asgi.py
@@ -10,18 +10,19 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tev.settings')
+import django
+django.setup()
+
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
 from main import routing
 import main.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tev.settings')
 
 django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": URLRouter(
         main.routing.websocket_urlpatterns
     )
