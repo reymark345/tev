d96db1f08a65a3c3c2d4d916cbfa9d8a5ad1751b
Reymark N. Valdehueza <dswd.focrg.devs@gmail.com>
2024-11-20 10:10:29 +0800

    ffasF

--- receive/views.py
+++ receive/views.py
@@ -1066,7 +1066,7 @@ def item_returned(request):
     id = request.POST.get('ItemID')
 
     data = TevIncoming.objects.filter(id=id).first()
-    tev_add = TevIncoming(code=data.code,first_name=data.first_name,middle_name=data.middle_name,last_name = data.last_name,id_no = data.id_no, account_no = data.account_no, date_travel = travel_date_spaces,original_amount=data.original_amount,final_amount = data.final_amount,incoming_in =date_time.datetime.now(),user_id=data.user_id)
+    tev_add = TevIncoming(code=data.code,first_name=data.first_name,middle_name=data.middle_name,last_name = data.last_name,id_no = data.id_no, account_no = data.account_no, date_travel = travel_date_spaces,original_amount=data.original_amount,final_amount = data.final_amount,incoming_in =date_time.datetime.now(),user_id=data.user_id, division = data.division, section = data.section)
     tev_add.save()
 
     last_added_tevincoming = TevIncoming.objects.latest('id')
